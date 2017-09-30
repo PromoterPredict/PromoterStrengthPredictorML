85e2af58e0ed319aef8ecaf0246dade11f47ac7b
Keshav Aditya R.P <keshavaditya26896@gmail.com>
2017-09-30 13:58:18 +0530

    Promoter Predict using Multivariate Linear Regression

---
+++ FinalP.py
@@ -0,0 +1,522 @@
+# Dependencies
+from __future__ import division
+import web
+import math
+import numpy as np
+from pylab import plot, show, xlabel, ylabel
+import random
+import web
+from Bio import motifs
+from Bio.Seq import Seq
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import time
+
+#                                           NOTE 
+#           HOW THE MODEL WORKS FOR PREDICTING AND ALLOWING THE USER TO ADD MORE DATASETS
+
+# OBTAIN ALL THE INPUTS FROM THE CLIENT AS A SINGLE STRING : Eg TGTGTGTGATGA*TGATGATGTGTG#0.5&
+# [THIS IS JUST A RANDOM SEQENCE TAKEN FOR THE SAKE OF THIS EXAMPLE]
+
+# EXAMPLE OF STRING SENT FROM SERVER : TGTGTGTGATGA*TGATGATGTGTG#0.5&
+
+# -35 AND -10 SEQUENCE THE USER ENTERS FOR WHICH HE REQUIRES THE PREDICTED STRENGTH (TGTGTGTGATGA*)
+# -35 SEQUENCE : TGTGTG
+# -10 SEQUENCE : TGATGA
+
+# SEPERATOR *
+
+# -35 AND -10 SEQUENCE THE USER ENTERS AS DYNAMIC INPUT WHICH IS TO BE ADDED TO THE DATA SET (TGATGATGTGTG#)   
+# -35 SEQUENCE : TGATGA
+# -10 SEQUENCE : TGTGTG
+# SUPPOSE THE USER ENTERS MORE THAN 1 DYNAMIC INPUT, LETS SAY 2 INPUT SETS (TGATGATGTGTGATGATGGATTGA*)
+# SEQUENCE 1 : TGATGATGTGTG
+# SEQUENCE 2 : ATGATGGATTGA
+# -35 SEQUENCE1 : TGATGA
+# -10 SEQUENCE1 : TGTGTG
+# -35 SEQUENCE2 : ATGATG
+# -10 SEQUENCE2 : GATTGA
+
+# SEPERATOR #
+
+# THE STRENGTH FOR THE RESPECTIVE -35 AND -10 SEQUENCE THE USER ENTERS AS DYNAMIC INPUT WHICH IS TO BE ADDED TO THE DATA SET(0.5&)
+# STRENGTH : 0.5
+# SUPPOSE THE USER ENTERS MORE THAN 1 DYNAMIC INPUT, LETS SAY 2 INPUT SETS (0.5&0.6&)
+# STRENGTH1 : 0.5
+# STRENGTH2 : 0.6
+
+# RETURNS RESUTS TO SERVER
+def make_text(string):
+    return string
+
+# TO CONNECT WITH THE WEBPAGE BEING USED
+urls = ('/', 'tutorial')
+render = web.template.render('templates/')
+
+app = web.application(urls, globals())
+
+my_form = web.form.Form(
+                web.form.Textbox('', class_='textfield', id='textfield'),
+                )
+
+class tutorial:
+    def GET(self):
+        form = my_form()
+        return render.tutorial(form, "")
+        
+    def POST(self):
+
+        # ALL THE VARIABLES USED
+        # CONSIDER EXAMPLE : TGTGTGTGATGA*TGATGATGTGTG#0.5&
+        form = my_form()
+        form.validates()
+        t = form.value['textfield']
+        numberOfElements = 0 # 2 x no of datasets added by user 
+        p = "" # Prediction Sequences -35 and -10 before * Eg TGTGTGTGATGA
+        s1 = "" # Prediction Sequence -35 TGTGTG
+        s2 = "" # Prediction Sequence -10 TGATGA
+        s = "" # User Added dataset -35 and -10 values 
+        out = "" # User Added dataset outputs with & between each 0.5
+        out2 = "" # Copy of out 
+        userData = [] # User added dataset in array form with -35 and -10 in alternative sequence  
+
+        # TEMPORARY VARIABLES - USED TO RETRIVE VAROUS PORTIONS OF THE STRING SENT FROM CLIENT    
+        sx = 0 # index of *
+        sy = 0 # index of # 
+        j = 6 # index of *
+        k = 0 
+        n = 0 
+        temp1 = 0
+        temp3 = 1.1
+        find = 0                 
+        temp2 = ""
+        outElements = []  
+        tempo = []
+        convert10 = []
+        convert35 = []
+        flag = 0 # Used to check if the user has entered 
+        
+        # DATA SET : 19 SAMPLES OF -35 AND -10 SEQUENCES WITH RESPECTIVE STRENGTH 
+
+        # -35 SEQUENCE
+        instances = [
+        Seq("TTGACG"),
+        Seq("TTTACA"),
+        Seq("TTGACA"),
+        Seq("CTGATA"),
+        Seq("TTGACA"),
+        Seq("TTTACG"),
+        Seq("TTTACG"),
+        Seq("TTTACG"),
+        Seq("CTGACA"),
+        Seq("TTTACA"),
+        Seq("TTTACG"),
+        Seq("TTGACG"),
+        Seq("CTGATA"),
+        Seq("CTGATG"),
+        Seq("TTTATG"),
+        Seq("TTTATA"),
+        Seq("TTGACA"),
+        Seq("TTGACA"),
+        Seq("TTGACG")
+        ]
+
+        # -10 SEQUENCE
+        instances2 =[
+        Seq("TACAGT"),
+        Seq("TATTAT"),
+        Seq("TACTGT"),
+        Seq("GATTAT"),
+        Seq("TATTGT"),
+        Seq("TACTAT"),
+        Seq("TATAGT"),
+        Seq("TATTAT"),
+        Seq("TATAAT"),
+        Seq("GACTGT"),
+        Seq("TACAAT"),
+        Seq("TATAGT"),
+        Seq("GATTAT"),
+        Seq("GATTAT"),
+        Seq("TACAAT"),
+        Seq("TACAAT"),
+        Seq("GACTAT"),
+        Seq("GATTGT"),
+        Seq("TATTGT")
+        ]            
+        
+        # STRENGTH OF -35 AND -10 SEQUENCE 
+        outputResult = [
+        [1.0],
+        [0.7],
+        [0.86],
+        [0.01],
+        [0.72],
+        [0.24],
+        [0.47],
+        [0.36],
+        [0.51],
+        [0.04],
+        [0.33],
+        [0.58],
+        [0.0],
+        [0.01],
+        [0.1],
+        [0.15],
+        [0.16],
+        [0.06],
+        [0.56]        
+        ]  
+        
+        # STRENGTH OF -35 AND -10 SEQUENCE  - COPY OF outputResult
+        outputResult2 = [
+        [1],
+        [0.7],
+        [0.86],
+        [0.01],
+        [0.72],
+        [0.24],
+        [0.47],
+        [0.36],
+        [0.51],
+        [0.04],
+        [0.33],
+        [0.58],
+        [0.01],
+        [0.01],
+        [0.1],
+        [0.15],
+        [0.16],
+        [0.06],
+        [0.56]
+        ]                   
+
+        # USED TO SLICE THE STRING TO OBTAIN 
+        # -35 AND -10 SEQUENCE FOR WHICH PREDICTION IS REQUIRED
+        # THE DYNAMIC DATASET ENTERED BY THE USER - IT'S -35 AND -10 SEQUENCE ALONG WITH IT'S STRENGTH 
+        sy = t.index('#')
+        sx = t.index('*')
+        p = str(t[0:sx])
+        if len(p) == 12:
+            s1 = str(t[0:6])
+            s2 = str(t[6:12])
+            flag = 1
+        s = str(t[sx+1:sy])
+        out = str(t[sy+1:])
+        out2 = str(t[sy+1:])
+        while k < len(s) :
+            userData.append(str(s[k:j]))
+            k += 6
+            j += 6
+            numberOfElements += 1
+
+        print "\t\t\t\t STRING SPLIT"
+        print t
+        print ""
+        print s1
+        print ""
+        print s2
+        print ""
+        print s
+        print ""
+        print out
+        print ""     
+        print userData
+        print ""
+        print numberOfElements  
+        print "" 
+
+        # GRADIENT DESCENT TTO OBTAIN THE THETA PARAMETER    
+        def gradientDescent(x, y, theta, alpha, m, numIterations):
+            J_history = np.zeros(shape=(numIterations, 1))
+            xTrans = x.transpose()
+            for i in range(0, numIterations):
+                hypothesis = np.dot(x, theta)
+                loss = hypothesis - y
+                cost = np.sum(loss ** 2) / (2 * m)
+                #print("Iteration %d | Cost: %f" % (i, cost))
+                gradient = np.dot(xTrans, loss) / m
+                theta = theta - alpha * gradient
+                J_history[i][0] = cost
+            return theta, J_history
+ 
+        print "\t\t\t\t -35 SEQUENCE"
+        print ""
+        
+        # TO APPEND THE -35 SEQUENCE DYNAMICALLY ADDED BY THE USER TO THE EXISTING DATASET 
+        i = 0
+        while i < numberOfElements:
+            if((i%2) == 0):
+                instances.append(Seq(userData[i]))
+            i += 1        
+
+        # CREATING A COPY OF INSTANCES ADDED WITH THE -35 SEQUENCE FOR WHICH WE HAVE TO PREDICT THE STRENGTH
+        if flag == 1:
+            instances.append(Seq(s1))
+            instancesP = instances[:]
+        
+        # CONVERTING THE -35 SEQUENCE INTO A SUITABLE FORMAT
+        i = 0
+        while i < len(instances):
+            convert35.append(str(instances[i]))
+            i+=1 
+        
+        # CONSTRUCTION OF THE PSSM MATRIX FOR THE -35 SEQUENCE BASED ON STRENGTH
+        m = motifs.create(instances)
+        #print(m.counts);
+        pwm = m.counts.normalize(pseudocounts= {'A':0.49, 'C': 0.51, 'G' : 0.51, 'T' : 0.49}                )
+        #print(pwm)
+        pssm = pwm.log_odds()
+        #print(pssm)
+
+        if flag == 1:
+            mP = motifs.create(instancesP)
+            pwmP = mP.counts.normalize(pseudocounts={'A':0.49, 'C': 0.51, 'G' : 0.51, 'T' : 0.49})
+            pssmP = pwmP.log_odds()
+            p,o,l,k,m,n = str(s1)
+            resultP = pssmP[p,0] + pssmP[o,1] + pssmP[l,2] + pssmP[k,3] + pssmP[m,4] + pssmP[n,5]       
+
+        result = []
+
+        def calculateX(a,b,c,d,e,f,x):
+            temp1 = pssm[a,0] + pssm[b,1] + pssm[c,2] + pssm[d,3] + pssm[e,4] + pssm[f,5]
+            result.append([temp1])
+
+        i = 0    
+ 
+        while i < len(convert35):
+            calculateX(convert35[i][0],convert35[i][1],convert35[i][2],convert35[i][3],convert35[i][4],convert35[i][5],i)
+            i +=1          
+        
+        # EXTRACT THE OUTPUT FROM THE DYNAMICALLY ENTERED USER DATASET AND STORING IT IN OUTPUTRESULTS
+        i = 0
+        counter = 0 
+        half = numberOfElements*0.5
+        while counter < half:
+            find = out.index('&')
+            temp2 = str(out[i:find])
+            temp3 = float(temp2)
+            outputResult.append([temp3])
+            out = out[find+1:] 
+            counter +=1
+
+        print "\t\t\t\t Obtaining input and output values for -35" 
+        print ""
+        print "\t\t\t\t X1 Values -35 Sequence"
+        print result                
+        print ""
+        print "\t\t\t\t Y Values Strength"
+        print outputResult 
+        print ""          
+       
+        print "\t\t\t\t -10"
+
+        # TO APPEND THE -10 SEQUENCE DYNAMICALLY ADDED BY THE USER TO THE EXISTING DATASET
+        i = 0
+        while i < numberOfElements:
+            if((i%2) != 0):
+                instances2.append(Seq(userData[i]))
+            i += 1        
+        
+        # CREATING A COPY OF INSTANCES ADDED WITH THE -10 SEQUENCE FOR WHICH WE HAVE TO PREDICT THE STRENGTH
+        if flag == 1:
+            instances2.append(Seq(s2))
+            instancesP2 = instances2[:]      
+        
+        # CONVERTING THE -10 SEQUENCE INTO A SUITABLE FORMAT
+        i = 0
+        while i < len(instances2):
+            convert10.append(str(instances2[i]))
+            i+=1
+        
+        # CONSTRUCTION OF THE PSSM MATRIX FOR THE -35 SEQUENCE BASED ON STRENGTH
+        m2 = motifs.create(instances2)
+        #print(m2.counts);
+        pwm2 = m2.counts.normalize(pseudocounts={'A':0.49, 'C': 0.51, 'G' : 0.51, 'T' : 0.49})
+        #print(pwm2)
+        pssm2 = pwm2.log_odds()
+        #print(pssm2)
+
+        if flag == 1:
+            mP2 = motifs.create(instancesP2)
+            pwmP2 = mP2.counts.normalize(pseudocounts={'A':0.49, 'C': 0.51, 'G' : 0.51, 'T' : 0.49})
+            pssmP2 = pwmP2.log_odds()
+            p2,o2,l2,k2,m2,n2 = str(s2)
+            resultP2 = pssmP2[p2,0] + pssmP2[o2,1] + pssmP2[l2,2] + pssmP2[k2,3] + pssmP2[m2,4] + pssmP2[n2,5]
+
+        result2 = []
+
+        def calculateX2(a,b,c,d,e,f,x):
+            temp1 = pssm2[a,0] + pssm2[b,1] + pssm2[c,2] + pssm2[d,3] + pssm2[e,4] + pssm2[f,5]
+            result2.append([temp1])
+
+        i = 0
+        while i < len(convert10):
+            calculateX2(convert10[i][0],convert10[i][1],convert10[i][2],convert10[i][3],convert10[i][4],convert10[i][5],i)
+            i +=1
+                      
+        # EXTRACT THE OUTPUT FROM THE DYNAMICALLY ENTERED USER DATASET AND STORING IT IN OUTPUTRESULTS2 (THIS IS A COPY OF OUTPUTRESULTS)
+        i = 0
+        counter = 0 
+        half = numberOfElements*0.5
+        while counter < half:
+            find = out2.index('&')
+            temp2 = str(out2[i:find])
+            temp3 = float(temp2)
+            outputResult2.append([temp3])
+            out2 = out2[find+1:] 
+            counter +=1 
+
+        print "\t\t\t\t Obtaining input and output values for -10" 
+        print ""
+        print "\t\t\t\t X2 Values -10 Sequence"
+        print result2         
+        print ""
+        print "\t\t\t\t Y Values Strength"
+        print outputResult2           
+       
+        # CONSTRUCTION OF A MATRIX - PSSM OF -35 AND -10 SEQUENCE
+        a = []
+        i = 0       
+        while i<len(outputResult):
+            a.append([1,result[i][0],result2[i][0]])
+            i +=1
+        
+        print ""
+        print "\t\t\t\t Matrix A (Input Matrix : -35 and -10 Sequence)"
+        for x in a:
+            print x
+            print ""
+
+        # CONSTRUCTION OF B MATRIX - STRENGTH OF -35 AND -10 SEQUENCE
+        b =[]
+        i = 0
+        while i<len(outputResult):
+              b += outputResult[i]
+              # TO DEAL WITH LOG(0)
+              if b[i] == 0:
+                  b[i] = 0.01 
+              b[i] = math.log(b[i])           
+              i +=1  
+        print ""
+        print "\t\t\t\t Matrix B (Strength)"       
+        for x in b:
+            print x
+            print ""
+        
+        # FORMATTING MATRIX A AND B TO OBTAIN MATRIX x AND y
+        print "\t\t\t\t Matrix X (Input Matrix : -35 and -10 Sequence)"
+        x = np.asarray(a)
+        print x
+        print ""
+        print "\t\t\t\t Matrix Y (Output Matrix : Strength)"
+        y = np.asarray(b)
+        print y
+        print ""
+
+        # CALLING THE GRADIENT DESCENT FUNCTION TO OBTAIN THE THETA PARAMETER
+        m, n = np.shape(x)
+        numIterations= 100000 #c
+        alpha = 0.015 #c
+        theta = np.ones(n)
+        theta, J_history = gradientDescent(x, y, theta, alpha,m,numIterations)
+        print "\t\t\t\t Theta : theta"
+        print(theta)
+        print ""
+
+        # CONSTRUCTING THE HYPOTHESIS 
+        hx = x.dot(theta)
+        print "\t\t\t\t Hypothesis"
+        print hx
+        print ""
+
+        # DIFFERENCE BETWEEN THE HYPOTHESIS AND OUTPUT MATRIX Y
+        print "\t\t\t\t Difference"
+        diff = hx - y
+        print diff
+        print ""
+
+        # DIFFERENCE SQUARED
+        print "\t\t\t\t Difference Square"
+        diff_square = diff*diff
+        print diff_square 
+        print ""
+
+        # SUM OF DIFFERENCES
+        print "\t\t\t\t Sum"
+        sum = np.sum(diff_square) 
+        print sum
+        print ""
+
+        # COST FUNCTION
+        print "\t\t\t\t Cost function"
+        temp = 1/(2*m)
+        cost = temp*sum
+        print cost
+        print ""
+        
+        # TO OBTAIN R SQUARE VALUE : CORRELATION COEFFICIENT
+        print "\t\t\t\t Cost Function"
+        meany = np.mean(y)
+        sumsqmeany = np.sum((y-meany)**2)
+        sumsqmeanysum = np.sum((y-hx)**2)/sumsqmeany
+        print sumsqmeanysum
+        print ""
+
+        print "\t\t\t\t R Sqare Value"
+        R = 1 - sumsqmeanysum
+        print R
+        print ""
+
+        # CONSTRUCTION OF THE MULTIVARIENT LINEAR REGRESSION GRAPH
+        fig =plt.figure()
+        ax = fig.add_subplot(111, projection = '3d')
+        for c, m in [('r','o')]:
+            xs = x[:,1]
+            print "\t\t\t\t xs Plot for Graph -10 Sequence"
+            print(xs)
+            print ""
+            ys = x[:,2]
+            print "\t\t\t\t ys Plot for Graph -35 Sequence"
+            print (ys)
+            print ""
+            zs = y
+            print "\t\t\t\t zs Plot for Graph Strength"
+            print (zs)
+            print ""
+            ax.scatter(xs, ys,zs, c=c, marker =m)
+        
+        ax.set_xlabel('-10 Hexamer')
+        ax.set_ylabel('-35 Hexamer')
+        ax.set_zlabel('Strength of Promoter')
+        
+        # TO SAVE THE GRAPH AS AN IMAGE
+        fig1 = plt.gcf()
+        # To display image in console : plt.show()
+        fig1.savefig('Multivariant.png', dpi=100)
+        data_uri = open('Multivariant.png', 'rb').read().encode('base64').replace('\n', '')      
+        graph = '<div class="row"><div class="col s12 m12 l12"><h2><center><i class="fa fa-line-chart fa-2x" aria-hidden="true"></i>&nbsp;&nbsp;Regression Model And Predicted Output</center></h2></div></div>'
+        img_tag = '<div class="row"><div class="col s12 m7 l7"><img id="reg" src="data:image/png;base64,%s"></div>' % data_uri
+        #print theta, J_history
+        plot(np.arange(numIterations), J_history)
+        xlabel('Iterations')
+        ylabel('Cost Function')
+        #show()
+
+        # TO SEND THE RESULTS BACK TO THE CLIENT
+        if flag == 1:
+            strength = np.array([1.0,   resultP, resultP2 ]).dot(theta)
+            finalStrength = strength
+            #print 'Predicted strength of promoter : %s' % (finalStrength)
+            #print 'Correlation Measure Sqare Value : %s' % (R)
+            pri = '<div class="col s12 m5 l5"><p class="para"><span class="highlight"><i class="fa fa-bullseye fa-2x" aria-hidden="true"></i></span><br><span class="highlight">Predicted Strength</span><br> %s </p><br><br>' % finalStrength
+            rsqare = '<p class="para"><span class="highlight"><i class="fa fa-gavel fa-2x" aria-hidden="true"></i></span><br><span class="highlight">Correlation Measure Sqare Value</span> <br> %s </p></div></div>' % R
+        else:
+            pri = '<div class="col s12 m5 l5"><p></p>'
+            rsqare = '<p class="para"><span class="highlight"><i class="fa fa-gavel fa-2x" aria-hidden="true"></i></span><br><span class="highlight">Correlation Measure Sqare Value</span> <br> %s </p></div></div>' % R
+           
+        return make_text(graph + img_tag + pri + rsqare)
+
+if __name__ == '__main__':
+    app.run()
+
+application = app.wsgifunc()
--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # PromoterStrengthPredictorML
 Machine Learning using Multivarient Linear Regression to Predict the Strength of a Promoter
+
+Quick summary
+
+This website serves as a Python based machine learning platform to predict the strength of sigma seventy core promoters in Escherichia coli in a manner that subverts the need for tedious experiments and is cost effective.
+Here multi-variate linear regression has been used where the parameters were optimized with gradient descent. The training data set used here is the Anderson promoter collection developed and characterized by the Anderson lab at UC Berkeley.
+
+How to Set it up
+
+General Dependencies
+
+* Python Version 2.7
+* Numpy
+* Biopython
+* Matplotlib
+
+Dependencies to Interface with the Client
+
+* Installation of Webpy a Python Framework
+* Nginx Server
+
+Contribution Guidelines
+
+* Finalp.py has the code which is fully interfaced with the web.
+
+* HOW THE MODEL WORKS FOR PREDICTING AND ALLOWING THE USER TO ADD MORE DATASETS
+
+* OBTAIN ALL THE INPUTS FROM THE CLIENT AS A SINGLE STRING : Eg TGTGTGTGATGA*TGATGATGTGTG#0.5&
+* [THIS IS JUST A RANDOM SEQENCE TAKEN FOR THE SAKE OF THIS EXAMPLE]
+
+* EXAMPLE OF STRING SENT FROM SERVER : TGTGTGTGATGA*TGATGATGTGTG#0.5&
+
+* -35 AND -10 SEQUENCE THE USER ENTERS FOR WHICH HE REQUIRES THE PREDICTED STRENGTH (TGTGTGTGATGA*)
+* -35 SEQUENCE : TGTGTG
+* -10 SEQUENCE : TGATGA
+
+* SEPERATOR *
+
+* -35 AND -10 SEQUENCE THE USER ENTERS AS DYNAMIC INPUT WHICH IS TO BE ADDED TO THE DATA SET (TGATGATGTGTG#)   
+* -35 SEQUENCE : TGATGA
+* -10 SEQUENCE : TGTGTG
+* SUPPOSE THE USER ENTERS MORE THAN 1 DYNAMIC INPUT, LETS SAY 2 INPUT SETS (TGATGATGTGTGATGATGGATTGA*)
+* SEQUENCE 1 : TGATGATGTGTG
+* SEQUENCE 2 : ATGATGGATTGA
+* -35 SEQUENCE1 : TGATGA
+* -10 SEQUENCE1 : TGTGTG
+* -35 SEQUENCE2 : ATGATG
+* -10 SEQUENCE2 : GATTGA
+
+* SEPERATOR #
+
+* THE STRENGTH FOR THE RESPECTIVE -35 AND -10 SEQUENCE THE USER ENTERS AS DYNAMIC INPUT WHICH IS TO BE ADDED TO THE DATA SET(0.5&)
+* STRENGTH : 0.5
+* SUPPOSE THE USER ENTERS MORE THAN 1 DYNAMIC INPUT, LETS SAY 2 INPUT SETS (0.5&0.6&)
+* STRENGTH1 : 0.5
+* STRENGTH2 : 0.6
+
+Owners :
+ 
+* Ashok Palaniappan
+* Ramit Kumar
+* Keshav Aditya R.P
